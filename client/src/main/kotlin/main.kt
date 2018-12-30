bb9c781af5d21980ca32bc12ef5864fbe0ed80fc
Ravensof <v@socialsib.net>
2018-12-30 21:23:32 +0700

    скрытие загрузочного экрана по окончанию загрузки скриптов

--- client/src/main/kotlin/main.kt
+++ client/src/main/kotlin/main.kt
@@ -4,11 +4,13 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import online.fivem.client.gtav.Natives
+import online.fivem.client.modules.nuiEventExchanger.NuiEvent
 import online.fivem.client.modules.nuiEventExchanger.NuiEventExchanger
 import online.fivem.client.modules.serverEventExchanger.ServerEventExchanger
 import online.fivem.common.GlobalConfig
 import online.fivem.common.common.Console
 import online.fivem.common.common.ModuleLoader
+import online.fivem.common.events.HideLoadingScreen
 import online.fivem.common.gtav.NativeEvents
 
 internal fun main(args: Array<String>) {
@@ -42,6 +44,8 @@ fun start() {
 //		add(Test())
 
 		finally {
+			NuiEvent.emit(HideLoadingScreen())
+
 			Console.log("all modules loaded")
 		}
 	}.start()
--- nui/src/main/kotlin/modules/loadingScreen/LoadingScreenModule.kt
+++ nui/src/main/kotlin/modules/loadingScreen/LoadingScreenModule.kt
@@ -1,7 +1,17 @@
 package online.fivem.nui.modules.loadingScreen
 
+import js.externals.jquery.jQuery
+import kotlinx.coroutines.Job
 import online.fivem.common.common.AbstractModule
+import online.fivem.common.events.HideLoadingScreen
+import online.fivem.nui.modules.clientEventEchanger.ClientEvent
 
 class LoadingScreenModule : AbstractModule() {
+	private val loadingScreenBlock = jQuery("#loadingScreen")
 
+	override fun start(): Job? {
+		ClientEvent.on<HideLoadingScreen> { loadingScreenBlock.hide() }
+
+		return super.start()
+	}
 }
