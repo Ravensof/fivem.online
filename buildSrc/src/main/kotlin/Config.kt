510833547ff4d77dd0ae74d3914880b8dc687ba4
v.kozlovsky <v.kozlovsky@magdv.com>
2019-03-13 15:33:19 +0700

    up serializer to 0.11.0-1.3.30-eap-11

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,7 +1,7 @@
 object Config {
 	const val kotlinVersion = "1.3.21"
 	const val coroutinesVersion = "1.1.1"
-	const val serializationRuntime = "0.10.0"
+	const val serializationRuntime = "0.11.0-1.3.30-eap-11"
 
 	object Ftp {
 		const val host = "192.168.1.5"
